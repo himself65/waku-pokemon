29cb3d48af10c28ae03f7951d2288a349348dbf4
Alex Yang <himself65@outlook.com>
2024-01-16 23:49:56 -0600

    fix: getEnv cause runtime error (#1)

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,9 @@
 import { getEnv } from "waku";
 import { neon } from "@neondatabase/serverless";
 
+// fixme: https://github.com/dai-shi/waku/issues/388
+// @ts-ignore
+globalThis.__WAKU_PRIVATE_ENV__ = process.env;
 const url = getEnv("DATABASE_URL");
 if (!url) {
   throw new Error("DATABASE_URL is not defined");
