3079f96d8b8597b2c00644ee61978f902eaf4819
Alex Yang <himself65@outlook.com>
2024-01-16 23:25:42 -0600

    fix: DATABASE_URL

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,7 +3,7 @@ import { neon } from "@neondatabase/serverless";
 
 const url = getEnv("DATABASE_URL");
 if (!url) {
-  throw new Error("WAKU_DATABASE_URL is not defined");
+  throw new Error("DATABASE_URL is not defined");
 }
 
 export const sql = neon(url);
